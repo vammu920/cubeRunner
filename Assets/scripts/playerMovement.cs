0e50dc77fd0615ac6272ace82a2f09807c15ea67
vamsi <vammu920@gmail.com>
2020-03-29 17:01:03 +0530

    enabled mobile mode

--- Assets/scripts/playerMovement.cs
+++ Assets/scripts/playerMovement.cs
@@ -7,7 +7,7 @@ public class playerMovement : MonoBehaviour
     public float sideForce = 300f;
 
     // Update is called once per frame
-    void FixedUpdate()
+        void FixedUpdate()
     {   
         rb.AddForce(0,0, forwardForce * Time.deltaTime, ForceMode.Force);
         if(Input.GetKey("d")){
@@ -21,6 +21,18 @@ public class playerMovement : MonoBehaviour
         }
         if(Input.GetAxis("Jump") != 0){
             rb.AddForce(Vector3.up * 10 * Time.deltaTime *80, ForceMode.Impulse);
+        }
+    }
+    void Update(){ 
+        if(Input.touchCount>0){
+            Touch touch = Input.GetTouch(0);
+            print(touch.position);
+            if(touch.position.x>500 ){
+                rb.AddForce(200 *Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+            }
+            if(touch.position.x<500){
+                rb.AddForce(-200 *Time.deltaTime, 0, 0,  ForceMode.VelocityChange);
+            }
         }
     }
 }
